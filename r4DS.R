711181361c3c260ee1128af27227db2b5294f831
Robert Szarek <szarekr@gmail.com>
2020-06-17 21:48:31 -0500

    More learning

--- r4DS.R
+++ r4DS.R
@@ -54,3 +54,139 @@ tribble(
 )
 
 df %>% .[["x"]]
+
+
+# Chapter 12 --------------------------------------------------------------
+
+stocks <- tibble(
+  year = c(2015, 2015, 2015, 2015, 2016, 2016, 2016),
+  qtr = c(1, 2, 3, 4, 2, 3, 4),
+  return = c(1.88, 0.59, 0.35, NA, 0.92, 0.17, 2.066)
+)
+
+stocks %>%
+  complete(year, qtr)
+
+
+# Chapter 13 --------------------------------------------------------------
+
+library(tidyverse)
+library(nycflights13)
+
+planes %>%
+  count(tailnum) %>%
+  filter(n > 1)
+
+weather %>%
+  count(year, month, day, hour, origin) %>%
+  filter(n > 1)
+
+flights2 <- flights %>%
+  select(year:day, hour, origin, dest, tailnum, carrier)
+flights2
+
+flights2 %>%
+  left_join(airlines, by = 'carrier')
+
+# Old base R method.
+flights2 %>%
+  select(-origin, -dest) %>%
+  mutate(name = airlines$name[match(carrier, airlines$carrier)])
+
+flights2 %>%
+  left_join(weather)
+
+flights2 %>%
+  left_join(airports, c("origin" = "faa"))
+
+# Lat/Lon plotting! Very neat.
+airports %>%
+  semi_join(flights, c("faa" = "dest")) %>%
+  ggplot(aes(lon, lat)) +
+  borders("state") +
+  geom_point() +
+  coord_quickmap()
+
+
+# 14 Strings --------------------------------------------------------------
+
+str_c(c("x", "y", "z"), collapse = ", ")
+
+x <- c("Apple", "Banana", "Pear")
+str_sub(x,1,3)
+
+str_sub(x, -3, -1)
+
+# Regex Tutorial
+x <- c("apple", "banana", "pear")
+str_view(x, "an")
+str(view(x, ".a."))
+
+name <- "Hadley"
+time_of_day <- "morning"
+birthday <- FALSE
+
+str_c(
+  "Good ", time_of_day, " ", name,
+  if (birthday) {" and HAPPY BIRTHDAY"}
+)
+
+
+str_c("prefix-", c("a", "b","c"), "-suffix")
+
+x <- c("Apple", "Banana", "Pear")
+str_sub(x, 1, 3)
+
+x <- "1888 is the longest year in Roman numerals: MDCCCLXXXVIII"
+str_view(x, "CC?")
+
+str_view(fruit, "(..)\\1", match = TRUE)
+
+
+# Factors -----------------------------------------------------------------
+
+x1 <- c("Dec", "Apr", "Jan", "Mar")
+
+
+month_levels <- c(
+  "Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+)
+
+y1 <- factor(x1, levels = month_levels)
+y1
+
+
+z <- c("Pass", "Did Not Pass")
+status_levels <- c("Pass", "Did Not Pass")
+
+r <- factor(z, levels = status_levels)
+
+gss_cat %>%
+  count(race)
+
+library(magrittr)
+gss_cat %>%
+  count(race)
+
+
+# Functions ---------------------------------------------------------------
+
+rescale01 <- function(x) {
+  rng <- range(x, na.rm = TRUE, finite = TRUE)
+  (x - rng[1]) / (rng[2] - rng[1])
+}
+rescale01(c(0, 5, 10))
+
+rescale01(c(-10,0,10))
+
+george <- function(x) {
+  
+  if (x == 1) {
+    next
+  } else {
+    print("Hoorah SOldier!")
+  }
+}
+
+
